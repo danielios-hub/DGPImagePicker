4895c78d1657004c4b2129f7bf242168aa1c498e
danielios-hub <daniel.ios.dgp@gmail.com>
2022-02-21 10:13:56 +0100

    Fix dependency url

--- Package.swift
+++ Package.swift
@@ -25,6 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DGPImagePicker",
-            dependencies: ["DGPLibrary", "DGPExtensionCore"]),
+            dependencies: ["DGPLibrary"]),
+//            dependencies: ["DGPLibrary", "DGPExtensionCore"]),
     ]
 )
